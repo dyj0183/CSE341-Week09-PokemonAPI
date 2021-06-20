46697d3f458e6c754d2abc525b02df590a85c278
Yu-Chun <dyj0183@byui.edu>
2021-06-20 00:07:32 -0600

    updated

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const app = express();
 app.set('view engine', 'ejs')
 .use(express.static(__dirname + '/public'))
 .use('/', require('./routes/route'))
-.listen(5000, () => {
+.listen(process.env.PORT||5000, () => {
     console.log("Listening on port 5000")
 });
